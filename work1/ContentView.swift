926e7d7fe2fe32de5d9360a3f53f7eae19086ecd
Jimmy <changchiyin@gmail.com>
2021-10-19 03:54:16 +0800

    Initial Commit

---
+++ work1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  work1
+//
+//  Created by ljy on 2021/10/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ work1/work1App.swift
@@ -0,0 +1,17 @@
+//
+//  work1App.swift
+//  work1
+//
+//  Created by ljy on 2021/10/19.
+//
+
+import SwiftUI
+
+@main
+struct work1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
